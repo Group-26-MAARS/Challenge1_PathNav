a6f2965538edf082f0214a7f1aef11ed9e50ccc3
Dylan Roberts <dtroberts@knights.ucf.edu>
2020-03-14 21:29:05 -0400

    Nav mesh now rebakes ever 1.5 seconds

---
+++ Assets/MoveTo.cs
@@ -0,0 +1,45 @@
+﻿// MoveTo.cs
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+
+    public Transform goal;
+    public Transform initial;
+
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+       
+        // Not necessary to set agent's destination here since it is set in Update()
+    }
+
+    void Update()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+
+        if (agent.isOnNavMesh)
+        {
+            if (!agent.pathPending)
+            {
+                if (agent.remainingDistance <= agent.stoppingDistance)
+                {
+                    if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                    {
+                        // Done
+                        //Debug.Log("navigation complete");
+                        agent.transform.position = initial.position;
+                        agent.destination = goal.position;
+
+                    }
+                }
+            }
+            else
+            {
+                Debug.Log("Agent is not on nav mesh (in update)");
+            }
+        }
+
+    }
+}
---
+++ Assets/NavMeshGen.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshGen : MonoBehaviour
+{
+    public NavMeshSurface surface;
+    public GameObject mixedRealityPlaySpace;
+    private bool navMeshIsCreated = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // StartCoroutine(buildNav());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // // Initially create Nav Mesh
+        if ((!navMeshIsCreated))
+        {
+            surface.BuildNavMesh();
+            navMeshIsCreated = true;
+        }
+        
+    }
+    IEnumerator buildNav()
+    {
+        while (true)
+        {
+            Debug.Log("Prior to buildig Navmesh");
+
+            yield return new WaitForSeconds(Convert.ToSingle(1.5)); // approx 1.5s
+
+            surface.BuildNavMesh();
+            Debug.Log("Rebulding Navmesh");
+        }
+
+    }
+}
