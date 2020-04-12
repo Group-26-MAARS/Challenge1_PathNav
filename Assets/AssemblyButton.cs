7183abb8428b19aab832ad0ceee9492452eac359
Steeve Joseph <steevejoseph@knights.ucf.edu>
2020-04-12 16:40:05 -0400

    MOdify  button to work w/ API

--- Assets/AssemblyButton.cs
+++ Assets/AssemblyButton.cs
@@ -10,6 +10,10 @@ using System.IO;
 using Newtonsoft.Json;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.HttpUtility;
+
 
 public class AssemblyButton : MonoBehaviour
 {
@@ -49,55 +53,81 @@ public class AssemblyButton : MonoBehaviour
     /// takes every project in there, and makes an object of the projects name, thingworxServer, url, and type (of Assembly)
     /// Saves the list of json to a file called "animations" (looking @ you, Dylan)
     /// </summary>
-    public void setupVuforiaStudioLogistics()
+    public async Task setupVuforiaStudioLogistics()
     {
-        string username = System.Environment.GetEnvironmentVariable("UserName");
-        string docDir = $"C:\\Users\\{username}\\Documents";
-        string challenge1Dir = $"{docDir}\\MAARS-C1";
-        string expDir = $"{challenge1Dir}\\Experiences";
-        string vuforiaProjectsDir = $"{docDir}\\VuforiaStudio\\Projects";
-
-        // for each dir in vuforiaProjectsDir (except node_modules)
-        string[] subdirs = Directory.GetDirectories(vuforiaProjectsDir);
-        subdirs = subdirs.Where(x => !x.Contains("node_modules")).ToArray();
-
-        string assembliesJsonPath = $"{challenge1Dir}\\animations.json";
-
-        List<Experience> assemblyList = new List<Experience>();
-
-        foreach (string sub in subdirs)
+        if (GetOS() == "WIN")
         {
-            string json = File.ReadAllText($"{sub}\\appConfig.json");
-            Experience tempExp = JsonUtility.FromJson<Experience>(json);
-            tempExp.url = WebUtility.UrlEncode($"https://view.vuforia.com/command/view-experience?url={tempExp.thingworxServer}/ExperienceService/content/projects/{tempExp.name}/index.html");
-            tempExp.type = "Assembly";
-            assemblyList.Add(tempExp);
-        }
+            string username = System.Environment.GetEnvironmentVariable("UserName");
+            string docDir = $"C:\\Users\\{username}\\Documents";
+            string challenge1Dir = $"{docDir}\\MAARS-C1";
+            string expDir = $"{challenge1Dir}\\Experiences";
+            string vuforiaProjectsDir = $"{docDir}\\VuforiaStudio\\Projects";
 
-        File.WriteAllText(assembliesJsonPath, JsonConvert.SerializeObject(assemblyList));
+            // for each dir in vuforiaProjectsDir (except node_modules)
+            string[] subdirs = Directory.GetDirectories(vuforiaProjectsDir);
+            subdirs = subdirs.Where(x => !x.Contains("node_modules")).ToArray();
+
+            string assembliesJsonPath = $"{challenge1Dir}\\animations.json";
+
+            List<Experience> assemblyList = new List<Experience>();
+
+            foreach (string sub in subdirs)
+            {
+                string json = File.ReadAllText($"{sub}\\appConfig.json");
+                Experience tempExp = JsonUtility.FromJson<Experience>(json);
+                tempExp.url = WebUtility.UrlEncode($"https://view.vuforia.com/command/view-experience?url={tempExp.thingworxServer}/ExperienceService/content/projects/{tempExp.name}/index.html");
+                tempExp.type = "Assembly";
+
+                HttpClient client = new HttpClient();
+
+                string body = $"{tempExp.name}:{JsonConvert.SerializeObject(tempExp)}";
+                NameValueCollection queryString = System.Web.HttpUtility.ParseQueryString(string.Empty);
+                queryString.Add("name", tempExp.name);
+                queryString.Add("url", tempExp.url);
+
+                string url = "https://sharingservice20200308094713.azurewebsites.net/api/animations";
+
+                // POST to API
+                var response = await client.PostAsync(url, new StringContent(queryString.ToString()));
+                //Debug.Log(response.StatusCode);
+                // end POST
+
+                assemblyList.Add(tempExp);
+            }
+
+            File.WriteAllText(assembliesJsonPath, JsonConvert.SerializeObject(assemblyList));
+        }
     }    
 
     /// <summary>
     /// Opens up a file browser so user can select the Experience they want
     /// Also calls runExperience()
     /// </summary>
-    public void selectProcedure()
+    public async void selectProcedure()
     {
-        setupVuforiaStudioLogistics();
-        string username = System.Environment.GetEnvironmentVariable("UserName");
+        if (GetOS() == "WIN")
+        {
+            await setupVuforiaStudioLogistics();
+            await setupDataLogistics();
+            string username = System.Environment.GetEnvironmentVariable("UserName");
 
-        string c1Dir = $"C:\\Users\\{username}\\Documents\\MAARS-C1\\";
-        string expDir = $"C:\\Users\\{username}\\Documents\\MAARS-C1\\Experiences";
+            string c1Dir = $"C:\\Users\\{username}\\Documents\\MAARS-C1\\";
+            string expDir = $"C:\\Users\\{username}\\Documents\\MAARS-C1\\Experiences";
 
-        // dirs created if not already existing
-        System.IO.Directory.CreateDirectory(c1Dir);
-        System.IO.Directory.CreateDirectory(expDir);
+            // dirs created if not already existing
+            System.IO.Directory.CreateDirectory(c1Dir);
+            System.IO.Directory.CreateDirectory(expDir);
 
-        // paths will be string[] of length 1 (at least).
-        var paths = StandaloneFileBrowser.OpenFilePanel("Open File", expDir, "", false);
-        //Debug.Log(String.Join("\n", paths));
+            // paths will be string[] of length 1 (at least).
+            var paths = StandaloneFileBrowser.OpenFilePanel("Open File", expDir, "", false);
+            //Debug.Log(String.Join("\n", paths));
+            runExperience(paths[0]);
+        }
+        else if (Application.platform == RuntimePlatform.Android)
+        {
 
-        runExperience(paths[0]);   
+        }
+
     }
 
 
@@ -136,7 +166,7 @@ public class AssemblyButton : MonoBehaviour
     /// (possibly when the process ends?)
     public void HandleAssembly(Experience exp)
     {
-        Debug.Log(exp);
+        //Debug.Log(exp);
         Application.OpenURL(exp.url);
     }
 
@@ -154,5 +184,23 @@ public class AssemblyButton : MonoBehaviour
         //    {
         //        Debug.Log($"{pair.Key}: {pair.Value}");
         //    }
+    }
+
+    public static string GetOS()
+    {
+        string os = "Unknow";
+        #if UNITY_EDITOR_WIN
+                    os = "WIN";
+        #endif
+        #if UNITY_IOS
+                    os = "IPHONE";
+        #endif
+        #if UNITY_ANDROID
+                    os = "ANDROID";
+        #endif
+        #if UNITY_WEBGL
+                    os = "WEBGL";
+        #endif
+        return os;
     }
 }
